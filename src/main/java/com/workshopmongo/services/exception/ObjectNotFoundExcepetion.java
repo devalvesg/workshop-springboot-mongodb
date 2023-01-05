b1042dfabaced1c5a33e9425b9f93f5fa921847d
devalvesg <gabrieldamasceno881@outlook.com>
2023-01-05 20:27:00 -0300

    Obtendo um usuario por id e tratando possiveis erros

---
+++ src/main/java/com/workshopmongo/services/exception/ObjectNotFoundExcepetion.java
@@ -0,0 +1,9 @@
+package com.workshopmongo.services.exception;
+
+public class ObjectNotFoundExcepetion extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundExcepetion(String msg) {
+		super(msg);
+	}
+}
