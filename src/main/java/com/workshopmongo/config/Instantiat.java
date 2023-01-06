7897f83adcc22ce24d332b4a7ef039054770f101
devalvesg <gabrieldamasceno881@outlook.com>
2023-01-06 11:43:42 -0300

    Acrescentando comentario aos posts

--- src/main/java/com/workshopmongo/config/Instantiat.java
+++ src/main/java/com/workshopmongo/config/Instantiat.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.workshopmongo.domain.Post;
 import com.workshopmongo.domain.User;
 import com.workshopmongo.dto.AuthorDTO;
+import com.workshopmongo.dto.CommentDTO;
 import com.workshopmongo.repository.PostRepository;
 import com.workshopmongo.repository.UserRepository;
 
@@ -40,10 +41,18 @@ public class Instantiat implements CommandLineRunner{
 		Post p1 = new Post(null,sdf.parse("21/03/2018") , "Partiu viagem!", "Vou viajar para SP, abraços!", new AuthorDTO(maria));
 		Post p2 = new Post(null,sdf.parse("23/03/2018") , "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO com1 = new CommentDTO("Boa viagem mano",sdf.parse("21/03/2018") , new AuthorDTO(alex));
+		CommentDTO com2 = new CommentDTO("Aproveite!",sdf.parse("22/03/2018") , new AuthorDTO(bob));
+		CommentDTO com3 = new CommentDTO("Tenha um otimo dia!",sdf.parse("23/03/2018") , new AuthorDTO(alex));
+				
+		p1.getComents().addAll(Arrays.asList(com1, com2));
+		p2.getComents().addAll(Arrays.asList(com3));
+		
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
 		maria.getPosts().addAll(Arrays.asList(p1, p2));
 		userRepository.save(maria);
+		
 	}
 
 }
--- src/main/java/com/workshopmongo/domain/Post.java
+++ src/main/java/com/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.workshopmongo.dto.AuthorDTO;
+import com.workshopmongo.dto.CommentDTO;
 @Document
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -19,6 +22,8 @@ public class Post implements Serializable{
 	private String body;
 	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -71,6 +76,10 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComents() {
+		return comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -87,5 +96,6 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
 
 }
